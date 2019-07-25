ea8c5bf137efa1d742ef0b2c158d46d444905055
sleigh <jj81927@gmail.com>
2019-07-25 12:03:24 +0800

    0725課堂上傳

--- sleigh.js
+++ sleigh.js
@@ -80,3 +80,30 @@ function creatTWIDall(area,gender){
     }
 
 }
+
+function createAnswer(n = 3){
+    let poker = [];
+    for(let i=0 ; i<10 ;i++) poker[i] = i;
+    for(let i=poker.length-1 ; i>0; i--){
+        let rand = parseInt(Math.random()*(i+1));
+        // i <==>rand 數值交換
+        [poker[i], poker[rand]] = [poker[rand], poker[i]];
+    }
+    let ret = '';
+    for(let i=0 ; i<n ; i++){   
+        ret = ret +poker[i];
+    }
+    return ret;
+}
+
+function checkAB(ans, gus){
+    let a = 0, b = 0;
+    for (let i=0; i<gus.length; i++){
+        if (gus.charAt(i) == ans.charAt(i)){
+            a++;
+        }else if (ans.indexOf(gus.charAt(i)) >= 0){
+            b++;
+        }
+    }
+    return `${a}A${b}B`;
+}
